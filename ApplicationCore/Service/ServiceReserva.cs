df62788f948d4b16a86fb7889b97a8a47da6ef39
RyuKing04 <tonyramirezs135@gmail.com>
2023-04-18 12:28:48 -0600

    Correo

--- ApplicationCore/Service/ServiceReserva.cs
+++ ApplicationCore/Service/ServiceReserva.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Mail;
+
 
 namespace ApplicationCore.Service
 {
@@ -32,6 +34,25 @@ namespace ApplicationCore.Service
         {
             IRepositoryReserva repository = new RepositoryReserva();
             return repository.Save(reserva);
+        }
+        public Reserva Correo (Reserva reserva)
+        {
+            // Configura el cliente SMTP
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.gmail.com"; // Cambia esto al servidor SMTP que desees usar
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential("tu-correo@gmail.com", "tu-contraseña");
+
+            // Crea el mensaje de correo electrónico
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("tu-correo@gmail.com");
+            message.To.Add("destinatario@ejemplo.com");
+            message.Subject = "Asunto del correo";
+            message.Body = "Contenido del correo electrónico.";
+
+            // Envía el correo electrónico
+            client.Send(message);
         }
     }
 }
